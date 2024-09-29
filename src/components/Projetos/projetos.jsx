a86cac9d6bbc421f08d200e1ba10000b2be6c447
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-09-29 15:46:56 -0300

    atualização do meu portifolio

--- src/components/Projetos/projetos.jsx
+++ src/components/Projetos/projetos.jsx
@@ -4,6 +4,8 @@ import projetos from "../dados/dadosProjetos";
 import Image from "next/image";
 import Link from "next/link";
 import gitHub from "../../../public/github.png"
+import seta from '../../../public/seta.png'
+import setaEsquerda from '../../../public/setaEsquerda.png'
 
 
 
@@ -46,6 +48,21 @@ export default function Projetos(props){
     },[])
 
 
+
+    function moveParaDireita(){
+
+        boxRef.current.scrollLeft += boxRef.current.offsetWidth 
+    }
+
+
+    function moveParaEsquerda(){
+
+        boxRef.current.scrollLeft =  boxRef.current.scrollLeft - boxRef.current.offsetWidth
+    }
+      
+
+
+
     return(
 
         <section id="idportifolio" className={`${estiloProj.boxPai} ${props.modoDiaProj}`}>
@@ -53,63 +70,50 @@ export default function Projetos(props){
                 <span>Navegue pelos meus projetos</span>
                 <h1 className={estiloProj.titulo}>Meus Projetos</h1>
 
-                <div ref={boxRef} className={`${estiloProj.boxProjetos}` }  >
+              
 
-                    {
-
-                        arrayProj.map((arrayProj)=>
-
-                            
-                        
-                            <div key={arrayProj.id} className={`${estiloProj.moldura} ${anima ? estiloProj.on : estiloProj.off}` }>
-
-                                
-
-                                <Image className={estiloProj.icon} src={arrayProj.img} alt="imagens" />
-                                <h1>{arrayProj.titulo}</h1>
-                                <p>{arrayProj.tx}</p>
-                                
-
-                                <div className={estiloProj.boxBtn}>
-                                    
-                                    <Link className={estiloProj.link} href={arrayProj.lk} target="_blank"  ><button>Ver Projeto</button></Link>
-
-                                   
-                                    <Link className={estiloProj.linkGit} href={arrayProj.gitlk} target="_blank"><Image className={estiloProj.iconGit} src={gitHub} /></Link>
-                                    
-                                </div>
-
-                                <section className={estiloProj.boxPaiTec}>
-
-                                    <span>Desenvolvido com :</span>
-
-                                    
-
-                                    <div className={estiloProj.boxTec}>
-
-
-                                        <Image alt={`imagem ${arrayProj.titulo}`}  src={arrayProj.tec} className={`${estiloProj.iconTec}  `}/>
-                                        <Image alt={`imagem ${arrayProj.titulo}`} src={arrayProj.tec2} className={estiloProj.iconTec}/>
-                                        <Image alt={`imagem ${arrayProj.titulo}`}  src={arrayProj.tec3} className={estiloProj.iconTec}/>
-                                        <Image alt={`imagem ${arrayProj.titulo}`} src={arrayProj.tec4} className={`${estiloProj.iconTec} ${arrayProj.id === 5 || arrayProj.id === 6 || arrayProj.id === 4 || arrayProj.id === 7 || arrayProj.id === 8 || arrayProj.id === 9 ? estiloProj.imgNone : estiloProj.imgBlock}`}/>
-                                    
+                    <div ref={boxRef} className={`${estiloProj.boxProjetos}` }  >
+                        {
+                            arrayProj.map((arrayProj)=>
+                    
+                    
+                                <div key={arrayProj.id} className={`${estiloProj.moldura} ${anima ? estiloProj.on : estiloProj.off}` }>
+                    
+                                    <Image className={estiloProj.icon} src={arrayProj.img} alt="imagens" />
+                                    <h1>{arrayProj.titulo}</h1>
+                                    <p>{arrayProj.tx}</p>
+                    
+                                    <div className={estiloProj.boxBtn}>
+                    
+                                        <Link className={estiloProj.link} href={arrayProj.lk} target="_blank"  ><button>Ver Projeto</button></Link>
+                    
+                                        <Link className={estiloProj.linkGit} href={arrayProj.gitlk} target="_blank"><Image className={estiloProj.iconGit} src={gitHub} /></Link>
+                    
                                     </div>
-                                </section>
-                                            
-
-
-                            </div>
-                        
-                        
-                        )
-
-
-                    }
-
-
-
-
-                </div>
+                                    <section className={estiloProj.boxPaiTec}>
+                                        <span>Desenvolvido com :</span>
+                    
+                                        <div className={estiloProj.boxTec}>
+                                            <Image alt={`imagem ${arrayProj.titulo}`}  src={arrayProj.tec} className={`${estiloProj.iconTec}  `}/>
+                                            <Image alt={`imagem ${arrayProj.titulo}`} src={arrayProj.tec2} className={estiloProj.iconTec}/>
+                                            <Image alt={`imagem ${arrayProj.titulo}`}  src={arrayProj.tec3} className={estiloProj.iconTec}/>
+                                            <Image alt={`imagem ${arrayProj.titulo}`} src={arrayProj.tec4} className={`${estiloProj.iconTec} ${arrayProj.id === 5 || arrayProj.id === 6 || arrayProj.id === 4 || arrayProj.id === 7 || arrayProj.id === 8 || arrayProj.id === 9 ? estiloProj.imgNone : estiloProj.imgBlock}`}/>
+                    
+                                        </div>
+                                    </section>
+                    
+                                </div>
+                    
+                    
+                            )
+                        }
+                    
+                                <div className={estiloProj.boxSetas}>
+                                    <Image onClickCapture={moveParaEsquerda}  className={estiloProj.setaDireita} src={setaEsquerda}/>
+                                    <Image  onClick={moveParaDireita} className={estiloProj.setaEsquerda} src={seta}/>
+                                </div>
+                    </div>
+              
 
 
 
